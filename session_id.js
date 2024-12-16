68fc92772cefc4d88e36698d039c59facce0cc99
Kingbabasdfgjkk <kb6431186@gmail.com>
2024-12-16 18:57:01 +0100

    Update session_id.js

--- session_id.js
+++ session_id.js
@@ -43,5 +43,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&vUsEyJ5T#p-2i0_4kpunz8ihbsWqQ3iUZJlrtv1R5iorXl_wGGF8",
 };
